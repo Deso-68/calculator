28f47dc5cbb1e7db7aab850079550212c2020467
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-14 17:25:49 +0800

    Add operateNumbers () to handle arithmetic operations

--- script.js
+++ script.js
@@ -9,6 +9,20 @@
 
 
 // Functions
+
+function operateNumbers(operator, first, second) {
+    switch (operator) {
+        case "+":
+            return addNumbers(first, second);
+        case "-":
+            return subtractNumbers(first, second);
+        case "*":
+            return multiplyNumbers(first, second);
+        case "/":  
+            return divideNumbers(first, second);
+    }
+}
+
 function addNumbers(first, second) {
     return first + second;
 }
